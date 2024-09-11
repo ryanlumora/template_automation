98a5f73aee5291b9273cc0fee1616eb45eb4e94f
Ryan Lucas <ryanlucaspatron@gmail.com>
2024-09-11 18:35:07 -0300

    Feat: criação da classe Login Logic com a lógica dos testes do sauce demo

---
+++ src/main/java/br/com/automation/selenium/saucedemo/logics/LoginLogic.java
@@ -0,0 +1,59 @@
+package br.com.automation.selenium.saucedemo.logics;
+
+import br.com.automation.selenium.saucedemo.pages.LoginPage;
+import lombok.extern.log4j.Log4j2;
+import org.junit.Assert;
+import static br.com.automation.configs.BrowserFactory.web;
+import static br.com.automation.configs.PDFGenerator.pdfGenerator;
+import static br.com.automation.utils.Utilities.*;
+
+@Log4j2
+public class LoginLogic {
+    private String step;
+    private final LoginPage page;
+
+    public LoginLogic(){
+        page = new LoginPage();
+    }
+
+    public void enterIntoSauceDemo() {
+        step = "Entrando na página de login do sauce demo";
+        web().get("https://www.saucedemo.com/v1/");
+        web().manage().window().maximize();
+
+        log.info(step);
+
+        pdfGenerator().takeScreenshot(web(), step);
+    }
+
+    public void insertFormsInformation(){
+        step = "Preenchendo informações do login";
+        log.info(step);
+
+        sleep();
+        page.getInputUser().sendKeys("standard_user");
+
+        sleep();
+        page.getInputPassword().sendKeys("secret_sauce");
+
+        pdfGenerator().takeScreenshot(web(), step);
+
+        sleep();
+        page.getInputUser().submit();
+
+    }
+
+    public void validateInventoryPage(){
+        step = "Validando que está na tela de pós login";
+
+        log.info(step);
+        sleep();
+
+        Assert.assertTrue(page.getInventory().isDisplayed());
+        Assert.assertTrue(web().getCurrentUrl().endsWith("/inventory.html"));
+
+        pdfGenerator().takeScreenshot(web(), "Validando que está na tela de pós login");
+    }
+
+
+}
